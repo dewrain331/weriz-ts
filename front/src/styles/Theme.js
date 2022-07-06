b74bb9428179c487a8cf4e084a986899785517c9
Isacc <dewrain331@gmail.com>
2022-07-06 15:41:07 +0900

    fix : styled-components base theme applied

---
+++ front/src/styles/Theme.js
@@ -0,0 +1,23 @@
+const colors = {
+  // black: "#333333"
+};
+
+const calcRem = (size) => `${size / 16}rem`;
+
+const fontSizes = {
+  xs: calcRem(12),
+  small: calcRem(14),
+  base: calcRem(16),
+  large: calcRem(18),
+  xl: calcRem(20),
+  xxl: calcRem(22),
+  titleSize: calcRem(50),
+  subTitleSize: calcRem(40),
+};
+
+const theme = {
+  colors,
+  fontSizes,
+};
+
+export default theme;
