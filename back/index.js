c3436a8831277ab1bea9c05b1ab63bc91653ae3d
Isacc <dewrain331@gmail.com>
2022-07-06 15:42:43 +0900

    fix : modify dotenv path

--- back/index.js
+++ back/index.js
@@ -3,7 +3,7 @@ import { app } from "./src/app";
 import path from "path";
 
 if (process.env.NODE_ENV === "local") {
-  dotenv.config({ path: path.join(__dirname, "../../.env.local") });
+  dotenv.config({ path: path.join(__dirname, "./.env.local") });
 }
 
 const PORT = +process.env.SERVER_PORT;
