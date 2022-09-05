66864ea341cbe971d1e8d4d4371453720117aae8
Isacc <dewrain331@gmail.com>
2022-09-06 07:52:22 +0900

    feat : Modal with Bootstrap
    - add Bootstrap package
    - rebuild Modal with Bootstrap components

--- front/src/component/modal/index.jsx
+++ front/src/component/modal/index.jsx
@@ -1,1 +1,19 @@
-import { useRef } from 'react';
+import { Modal } from "react-bootstrap";
+
+import ModalPortal from "./ModalPortal";
+
+const ModalComp = ({ show, setShow, title, main, children }) => {
+  return (
+    <ModalPortal>
+      <Modal show={show} onHide={() => setShow(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>{title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{main}</Modal.Body>
+        <Modal.Footer>{children}</Modal.Footer>
+      </Modal>
+    </ModalPortal>
+  );
+};
+
+export default ModalComp;
--- front/src/component/modal/modalPortal.js
+++ front/src/component/modal/modalPortal.js
@@ -1,8 +1,8 @@
-import ReactDom from 'react-dom';
+import ReactDom from "react-dom";
 
-const ModalPortal = ({children}) => {
+const ModalPortal = ({ children }) => {
   const el = document.getElementById("modal-root");
   return ReactDom.createPortal(children, el);
-}
+};
 
 export default ModalPortal;
