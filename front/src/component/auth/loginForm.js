fb8955742b2a8cc8f064ab65e5c71977da57d7aa
Isacc <dewrain331@gmail.com>
2022-09-12 21:28:40 +0900

    feat : auth.style.js
    - ing...

--- front/src/component/auth/loginForm.js
+++ front/src/component/auth/loginForm.js
@@ -5,6 +5,10 @@ import Api from "../../api";
 const LoginForm = ({ setShow }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const submitLoginForm = async (evt) => {
+    evt.preventDefault();
+  };
 };
 
 export default LoginForm;
