a9d0aee8b2924ccc9a2c256a8511926dc2f9dc88
KimKiDong <97578758+dewrain331@users.noreply.github.com>
2022-09-13 21:40:34 +0900

    chore : Capital Letter Issue
    - Concerning errors, I modified this file in Github.

--- front/src/styles/layout.jsx
+++ front/src/styles/layout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 
-import Header from "../component/header";
+import Header from "../components/header";
 
 const Layout = () => {
   return (
