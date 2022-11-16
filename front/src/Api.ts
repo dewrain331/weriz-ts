a917b0e0a2dd6f317b64a8f23dc010fb35ad8901
Isacc <dewrain331@gmail.com>
2022-11-16 15:57:13 +0900

    feat : Api.js => Api.ts
    - apply typescript grammar

--- front/src/Api.ts
+++ front/src/Api.ts
@@ -1,8 +1,25 @@
 /* eslint-disable */
-
 import axios from "axios";
+import type {
+  AxiosRequestConfig,
+  AxiosInterceptorManager,
+  AxiosInstance,
+  AxiosResponse,
+} from "axios";
 
-const Api = axios.create({
+type CustomResponseFormat<T = any> = {
+  response: T;
+  refreshedToken?: string;
+};
+
+interface CustomInstance extends AxiosInstance {
+  interceptors: {
+    request: AxiosInterceptorManager<AxiosRequestConfig>;
+    response: AxiosInterceptorManager<AxiosResponse<CustomResponseFormat>>;
+  };
+}
+
+const Api: CustomInstance = axios.create({
   baseURL: `http://${window.location.hostname}:5001`,
   timeout: 10000, // ms
 });
@@ -12,8 +29,10 @@ Api.interceptors.request.use(
   async (config) => {
     const accessToken = localStorage.getItem("accessToken");
 
-    config.headers["Content-Type"] = "application/json, charset=utf-8";
-    accessToken && (config.headers["Authorization"] = `Bearer ${accessToken}`);
+    config.headers = {
+      "Content-Type": "application/json, charset=utf-8",
+      Authorization: `Bearer ${accessToken}`,
+    };
 
     return config;
   },
